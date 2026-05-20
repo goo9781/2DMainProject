ee3e309edadc5e5b6f3a913b964b55ea9a70bd35
MinGeon <goo9781@gmail.com>
2026-05-20 18:19:36 +0900

    [Battlesystem] 플레이어/플레이어 공격 스크립트 수정
    플레이어 스크립트에 입력한 공격 관련 스크립트를 2DPlayerAttack 스크립트로 이동

--- Assets/Scripts/MG_2D/MG_2DPlayerAttack.cs
+++ Assets/Scripts/MG_2D/MG_2DPlayerAttack.cs
@@ -1,17 +1,28 @@
-﻿using System.Runtime.CompilerServices;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MG_2DPlayerAttack : MonoBehaviour
 {
+    [Header("애니메이터")]
+    [SerializeField] private Animator Animator_Entity;
+    
     [Header("공격 설정")]
     [SerializeField] private Transform _attackPoint;
     [SerializeField] private float _attackRadius = 0.7f;
     [SerializeField] private int _attackDamage = 10;
     [SerializeField] private LayerMask _monsterLayer;
+    [SerializeField] private KeyCode _attackKey = KeyCode.J;
+    [SerializeField] private float _attackCoolTime = 0.5f;
+
+    private float _attackTimer;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.J))
+       if (_attackTimer > 0f)
+        {
+            _attackTimer -= Time.deltaTime;
+        }
+        
+        if (Input.GetKeyDown(_attackKey))
         {
             Attack();
         }
@@ -19,11 +30,20 @@ public class MG_2DPlayerAttack : MonoBehaviour
 
     private void Attack()
     {
+        if (_attackTimer > 0f)
+        {
+            return;
+        }
+
+        _attackTimer = _attackCoolTime;
+
+        PlayAttackAnimation();
+
         Collider2D hitCollider = Physics2D.OverlapCircle(
             _attackPoint.position,
             _attackRadius,
             _monsterLayer
-            );
+        );
 
         if (hitCollider == null)
         {
@@ -40,6 +60,16 @@ public class MG_2DPlayerAttack : MonoBehaviour
         Debug.Log("플레이어 공격 성공");
 
         monster.TakeDamage(_attackDamage);
+    }
+
+    private void PlayAttackAnimation()
+    {
+        if (Animator_Entity == null)
+        {
+            return;
+        }
+
+        Animator_Entity.SetTrigger("Attack");
     }
 
         private void OnDrawGizmos()
