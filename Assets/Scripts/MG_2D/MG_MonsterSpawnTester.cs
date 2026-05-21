4f023f95b0d8dcfd96b3f201997975c98c85ac69
MinGeon <goo9781@gmail.com>
2026-05-21 17:20:28 +0900

    [GameObjectManager] 몬스터 스폰 테스터 추가
    오브젝트매니저를 통해 정상적으로 소환되는지 확인 완료

---
+++ Assets/Scripts/MG_2D/MG_MonsterSpawnTester.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MG_MonsterSpawnTester : MonoBehaviour
+{
+    [SerializeField] private Transform Transform_SpawnPoint;
+    [SerializeField] private KeyCode _spawnKey = KeyCode.M;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(_spawnKey))
+        {
+            SpawnMonster();
+        }
+    }
+
+    private void SpawnMonster()
+    {
+        if (MGGameObjectManager.Inst == null)
+        {
+            Debug.LogWarning("MGGameObjectManager가 없습니다.");
+            return;
+        }
+
+        Vector3 spawnPosition = transform.position;
+
+        if (Transform_SpawnPoint != null)
+        {
+            spawnPosition = Transform_SpawnPoint.position;
+        }
+
+        MGGameObjectManager.Inst.RequestSpawnMonster(spawnPosition);
+    }
+}
