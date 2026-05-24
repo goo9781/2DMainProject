b5a789d658a1814b124d1c5e05706c2918ecdbca
MinGeon <goo9781@gmail.com>
2026-05-24 16:29:42 +0900

    [데이터드리븐] GameData 스크립트 추가
    게임 내 데이터를 관리할 GameDataBase 클래스 추가 완료

---
+++ Assets/Scripts/MG_DataDriven/MGGameData.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class GameDataBase
+{
+    public string Id;
+}
+
+[System.Serializable]
+public class MGMonsterData : GameDataBase
+{
+    public string Name;
+    public string Descreiption;
+    public string IconPath;
+    public string PrefabPath;
+
+    public int MaxHp;
+    public int AttackDamage;
+
+    public float MoveSpeed;
+    public float PatrolSpeed;
+    public float PatrolRange;
+
+    public float DetectRange;
+    public float AttackRange;
+    public float AttackCoolTime;
+}
